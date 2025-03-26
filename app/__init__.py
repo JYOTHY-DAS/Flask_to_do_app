6deb1561bdfa26c09dac67bbbbf8deb25e26de8a
Jyothy Das <jyothydas11@gmail.com>
2025-03-26 17:00:10 +0530

    Comments updated for run and __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,29 @@
-from flask import Flask
+from flask import Flask #importing the Flask class from the flask module.
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 # Setup Flask app
-app = Flask(__name__)
+# Flask(__name__) creates an instance of the Flask class.
+app = Flask(__name__) #app is now a Flask application object that we can use to define routes, handle requests, etc.
 
 # Configure SQLite database
+'''
+* Configures SQLAlchemy to use an SQLite database named todo.db 
+* Three slashes mean relative path (database is in the project folder) '''
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+
+# Prevents Flask-SQLAlchemy from tracking changes to objects to save memory.
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize database
+'''
+* Creates a database connection linked to the Flask app.
+* db will be used to define database models and interact with the database.'''
+
 db = SQLAlchemy(app)
 
 # Import routes (after app & db to avoid circular imports)
+'''If we import routes at the beginning, routes.py might try to import app before it's fully created.
+Importing it after app and db helps to avoids circular import issues.'''
 from app import routes
--- run.py
+++ run.py
@@ -1,4 +1,18 @@
-from app import app
+from app import app #Imports the app object from the app module (which refers to app/__init__.py).
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+'''What Does __name__ == '__main__' Mean?
+* In Python, every script/module has a special built-in variable called __name__.
+* When you run a script directly, __name__ is set to "__main__".
+* When you import a script as a module, __name__ is set to the module name instead.
+* if __name__ == '__main__': checks whether the script is being run directly (not imported as a module).
+* It ensures that app.run(debug=True) is executed only when you run python run.py.
+'''
+
+'''
+* app.run(debug=True) Starts the Flask development server.
+* The debug=True flag enables debug mode, so the app automatically reloads when you make code changes 
+and displays detailed error messages in the browser if something goes wrong.
+'''
